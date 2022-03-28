5c2041ca271a9f6fec8d7a798e29e7d8dcf15d00
martinesk <jobsmartines@gmail.com>
2022-03-28 03:25:55 -0500

    add new projects

---
+++ image_facial_emotion_rec/streamlit_imgfacerec_main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from ImageFacialRec import *
+
+# fig = plt.figure(figsize=(20, 20))
+#
+# for i in range(8):
+#     ax = fig.add_subplot(4, 2, i + 1)
+#     # Using squeeze to convert the image shape from (96,96,1) to (96,96)
+#     plt.imshow(X_test[i].squeeze(),cmap='gray')
+#     for j in range(1,31,2):
+#             plt.plot(df_predict.loc[i][j-1], df_predict.loc[i][j], 'rx')
+#
+# plt.show()
+# st.pyplot()
+
+st.header('Some')
