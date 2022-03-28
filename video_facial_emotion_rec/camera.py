1cddcdb18b8851eaebb80f448cc48ad72843f240
martinesk <jobsmartines@gmail.com>
2022-03-28 03:37:59 -0500

    add new projects

--- video_facial_emotion_rec/camera.py
+++ video_facial_emotion_rec/camera.py
@@ -2,14 +2,14 @@ import cv2
 from model import FacialExpressionModel
 import numpy as np
 
-facec = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-model = FacialExpressionModel("model.json", "model_weights.h5")
+facec = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
+model = FacialExpressionModel("./model.json", "./model_weights.h5")
 font = cv2.FONT_HERSHEY_SIMPLEX
 
 class VideoCamera(object):
     def __init__(self):
         self.video = cv2.VideoCapture('./videos/presidential_debate.mp4')
-        
+
 
     def __del__(self):
         self.video.release()
